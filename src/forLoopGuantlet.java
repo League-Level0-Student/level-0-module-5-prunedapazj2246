3489964a7af4d187efb09ccb6e5bc9ffd33eb40a
League Student <league@iMac-17.attlocal.net>
2019-08-12 16:26:01 -0700

    For Loop Guantlet (IP)

---
+++ src/forLoopGuantlet.java
@@ -0,0 +1,62 @@
+
+public class forLoopGuantlet {
+
+	public static void main(String[] args) {
+/*int age=0;		// TODO Auto-generated method stub
+for (int i = 0; i<=100; i++) {
+	System.out.println(i);
+}
+for (int j = 100; j >= 0; j--) {
+	System.out.println(j);
+}
+for (int k = 2; k <= 100; k+=2) {
+System.out.println(k);	
+}
+for (int a = 1; a <= 99; a+=2) {
+	System.out.println(a);
+}
+for (int b = 1; b <= 500; b++) {
+if(b%2==1) {
+	System.out.println(b +"is odd");
+}else {
+	System.out.println(b+" is even");
+}
+}
+for (int c = 0; c <= 777; c+=7) {
+System.out.println(c);	
+}
+for (int d = 2005; d <= 2019; d++) {
+
+
+	
+
+System.out.println("In "+d+" I was "+age+" years old!");
+age+=1;
+}*/
+/*for (int i = 0; i < 7; i+=3) {
+	
+for (int j = 1; j < 4; j++) {
+	System.out.print(j+i);
+	
+}
+System.out.println("");
+}*/
+
+/*for (int i = 0; i < 3; i++) {
+for (int j = 0; j <= 2; j++) {
+	System.out.println(i+" "+j);
+}
+
+}*/
+for (int i = 1; i <= 10; i++) {
+	
+	for (int j = 1; j <= 100; j+=20) {
+	System.out.print(i+""+j);	
+	}
+	System.out.println("");
+}
+
+}
+	}
+
+
